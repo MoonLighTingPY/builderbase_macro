0fe285395be4d7cb597ce5d2591d4ae54ce3ee29
MoonLighTingPY <romanfigelo@gmail.com>
2025-06-30 00:38:14 +0300

    update build.bat

--- build.bat
+++ build.bat
@@ -11,11 +11,5 @@ if exist build rmdir /s /q build
 REM Build with PyInstaller (add icon)
 pyinstaller --onefile --noconsole --add-data "2k;2k" --add-data "fullhd;fullhd" --icon "coc-icon.ico" --name "builderbase_farmer" main.py
 
-REM Copy README and requirements.txt for distribution
-if exist dist (
-    copy README.md dist\
-    copy requirements.txt dist\
-)
-
 echo Build complete! Find builderbase_farmer.exe in the dist\ folder.
 pause
