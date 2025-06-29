e00300de961bef99be5553462036a811b9063bc6
MoonLighTingPY <romanfigelo@gmail.com>
2025-06-29 21:10:32 +0300

    ..

--- overlay_status.py
+++ overlay_status.py
@@ -10,28 +10,32 @@ class OverlayStatus:
     ALPHA = 0.75
 
     def __init__(self, width=220, height=38):
+        pass
 
 
     def _set_position(self):
+        pass
 
 
     def show(self, text, color="default"):
+        pass
 
 
     def hide(self):
-
+        pass
 
     def destroy(self):
-
+        pass
 
 
 def get_overlay():
-
+    pass
 
 def update_overlay_status(text, color="default", root=None):
-
+    pass
 
 def hide_overlay_status():
-
+    pass
 
 def destroy_overlay_status():
+    pass
